2b6fa9ff5b0c3258644c9cf498bc43140ff2481b
Naveen AutomationLabs <naveenanimation20@gmail.com>
2020-03-23 08:25:13 +0530

    Create SigupPage.java
    addded sign up page

---
+++ src/main/java/com/qa/hubspot/pages/SigupPage.java
@@ -0,0 +1,17 @@
+package com.qa.hubspot.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.qa.hubspot.base.BasePage;
+
+public class SigupPage extends BasePage{
+	
+	WebDriver driver;
+	public SigupPage(WebDriver driver){
+		this.driver = driver;
+	}
+	
+	
+	
+
+}
